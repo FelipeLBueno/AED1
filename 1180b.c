50402cedbe538afb7db4e396ebafecb2dea3ff1c
FelipeLBueno <felipe.bueno@unifesp.br>
2025-09-09 17:13:33 -0300

    Update 1180b.c

--- 1180b.c
+++ 1180b.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 
 int main() {
-    int N;
-    scanf("%d", &N);
+    int n;
+    scanf("%d", &n);
 
-    int X[N];  // vetor normal para armazenar os valores
-    int *p = X; //ponteiro que aponta para o início do vetor
+    int V[n];  // vetor normal para armazenar os valores
+    int *p = V; //ponteiro que aponta para o início do vetor
 
     // leitura dos elementos usando ponteiro
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < n; i++) {
         scanf("%d", (p + i));
     }
 
     int menor = *p;   // valor do primeiro elemento
-    int pos = 0;
+    int posicao = 0;
 
     // percorre usando ponteiro
-    for (int i = 1; i < N; i++) {
+    for (int i = 1; i < n; i++) {
         if (*(p + i) < menor) {   // acessa o valor com *(p+i)
             menor = *(p + i);
-            pos = i;
+            posicao = i;
         }
     }
 
     printf("Menor valor: %d\n", menor);
-    printf("Posicao: %d\n", pos);
+    printf("Posicao: %d\n", posicao);
 
     return 0;
+
 }
